05f60aabb2d177209cf6e773683500ad3190e2c1
zteffano <48279765+zteffano@users.noreply.github.com>
2023-08-23 12:26:05 +0200

    Update af Table struktur til dynamisk

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const cities = ['East Harlem', 'Brooklyn', 'Manhattan', 'Queens'];
+const drugsList = ['cocaine', 'heroin', 'ecstacy', 'lsd', 'hash'];
+
 const prices = {
     cocaine: { min: 4000, max: 12000 },
     heroin: { min: 5000, max: 15000 },
@@ -5,65 +8,71 @@ const prices = {
     lsd: { min: 1500, max: 3500 },
     hash: { min: 1000, max: 4000 }
 };
-let players = [];
-let currentPlayerIndex = 0; 
 
+let players = [];
+let currentPlayerIndex = 0;
 
 class Player {
     constructor(name) {
         this.name = name;
     }
 }
+
 function rotateTurns() {
     currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
-    document.getElementById('playerTurn').textContent = `It's ${players[currentPlayerIndex].name}'s turn!`;
+    if(players.length > 0 && players[currentPlayerIndex]) {
+        document.getElementById('playerTurn').textContent = `It's ${players[currentPlayerIndex].name}'s turn!`;
+        console.log(players)
+    }
 }
 
-
-
-// On page load, populate the players list and set the first player's turn
 window.onload = function() {
     document.querySelectorAll('.player-name').forEach(input => {
         players.push(new Player(input.value || input.placeholder));
     });
-    document.getElementById('playerTurn').textContent = `It's ${players[currentPlayerIndex].name}'s turn!`;
+    if(players.length > 0 && players[currentPlayerIndex]) {
+        document.getElementById('playerTurn').textContent = `It's ${players[currentPlayerIndex].name}'s turn!`;
+    }
+    generateTableHeaders();
+    generateTable();
 }
-
 
 function getRandomPrice(min, max) {
     let rawPrice = Math.random() * (max - min + 1) + min;
-    // Round the price to the nearest 10
     return Math.round(rawPrice / 100) * 100;
 }
-
 
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
 function updatePrices() {
-    for (let drug in prices) {
-        document.querySelectorAll(`.${drug}`).forEach(cell => {
-            cell.textContent = `$${getRandomPrice(prices[drug].min, prices[drug].max)}`;
-        });
+    console.log(`Players Array: ${players}`)
+    console.log(`Current Player Index: ${currentPlayerIndex}`)
+    for (let drug of drugsList) {
+        for (let city of cities) {
+            let cell = document.querySelector(`.${drug}.${city.toLowerCase().replace(/\s+/g, '-')}`);
+            if(cell) {
+                cell.textContent = `$${getRandomPrice(prices[drug].min, prices[drug].max)}`;
+            } else {
+                console.error(`Cell for drug: ${drug} and city: ${city} not found.`);
+            }
+        }
     }
 }
 
 document.getElementById('startGame').addEventListener('click', function() {
     let numPlayers = document.getElementById('playerSelect').value;
-    
-    // Show player input boxes
+
     document.querySelectorAll('.player-input').forEach((container, index) => {
         if (index < numPlayers) {
             container.style.display = 'block';
         }
     });
-    
-    // Hide the start game container
+
     document.getElementById('startContainer').style.display = 'none';
 });
 
-// On start of the game (when Roll Dice is clicked), initialize players.
 document.getElementById('rollDice').addEventListener('click', function() {
     if (players.length === 0) {
         document.querySelectorAll('.player-name').forEach(input => {
@@ -71,7 +80,9 @@ document.getElementById('rollDice').addEventListener('click', function() {
                 players.push(new Player(input.value || input.placeholder));
             }
         });
-        document.getElementById('playerTurn').textContent = `It's ${players[currentPlayerIndex].name}'s turn!`;
+        if(players.length > 0 && players[currentPlayerIndex]) {
+            document.getElementById('playerTurn').textContent = `It's ${players[currentPlayerIndex].name}'s turn!`;
+        }
     }
 
     const diceVal = rollDice();
@@ -80,35 +91,57 @@ document.getElementById('rollDice').addEventListener('click', function() {
     rotateTurns();
 });
 
-
-
-// Initial Price Update
-updatePrices();
-
-
 document.querySelectorAll('.player-name-field').forEach(inputField => {
     inputField.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-            // Prevent form submission if it's inside a form
             event.preventDefault();
 
-            // Get the entered name
             let enteredName = inputField.value;
-
-            // Find the related containers
             let inputContainer = inputField.closest('.player-name-input');
             let nameLabelContainer = inputField.closest('.player-input').querySelector('.player-name-label');
             let nameTextElement = nameLabelContainer.querySelector('.player-name-text');
 
-            // Update text and swap visibility
             nameTextElement.textContent = enteredName;
             inputContainer.style.display = 'none';
             nameLabelContainer.style.display = 'block';
 
-            // Update the players list
             if (!players.some(p => p.name === enteredName)) {
                 players.push(new Player(enteredName));
             }
         }
     });
 });
+
+function generateTable() {
+    let table = document.querySelector('tbody');
+    table.innerHTML = '';
+
+    for (let drug of drugsList) {
+        let row = document.createElement('tr');
+        let drugCell = document.createElement('td');
+        drugCell.innerHTML = `<b>${drug.charAt(0).toUpperCase() + drug.slice(1)}</b>`;
+        row.appendChild(drugCell);
+
+        for (let city of cities) {
+            let cell = document.createElement('td');
+            cell.classList.add(drug, city.toLowerCase().replace(/\s+/g, '-'));
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
+}
+
+function generateTableHeaders() {
+    let headerRow = document.querySelector('thead tr');
+    
+    while (headerRow.children.length > 1) {
+        headerRow.removeChild(headerRow.lastChild);
+    }
+
+    for (let city of cities) {
+        let header = document.createElement('th');
+        header.textContent = city;
+        headerRow.appendChild(header);
+    }
+}
